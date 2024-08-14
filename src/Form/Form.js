32a465e0058c51b93ca91e68bcd9a9c38c33c3cd
verocentenop <159185910+verocentenop@users.noreply.github.com>
2024-08-14 10:00:13 +0200

    Add files via upload

---
+++ src/Form/Form.js
@@ -0,0 +1,41 @@
+// form.js
+import { printRecords } from '../Records/Records'
+
+export const printForm = () => {
+  const appContainer = document.querySelector('#app')
+  const formContainer = document.createElement('div')
+  formContainer.className = 'form-container'
+
+  const form = document.createElement('form')
+  form.className = 'register-form'
+
+  const label = document.createElement('label')
+  label.textContent = 'Introduce tu nombre:'
+
+  const input = document.createElement('input')
+  input.type = 'text'
+  input.id = 'username'
+  input.required = true
+
+  const submitButton = document.createElement('button')
+  submitButton.type = 'submit'
+  submitButton.textContent = 'Registrar'
+
+  form.append(label, input, submitButton)
+  formContainer.append(form)
+  appContainer.append(formContainer)
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const username = input.value.trim()
+    if (username) {
+      localStorage.setItem('username', username)
+
+      // Actualizar los records del nuevo usuario
+      printRecords()
+
+      // Opcionalmente, se podría ocultar el formulario después de registrar el usuario
+      formContainer.remove()
+    }
+  })
+}
