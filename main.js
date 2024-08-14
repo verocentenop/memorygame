94623501ade8fa7cc4b5182592f9a666687be9c1
verocentenop <159185910+verocentenop@users.noreply.github.com>
2024-08-14 09:57:57 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,11 +3,13 @@ import { printCartas } from './src/Cartas/Cartas'
 import { printHeader } from './src/Header/Header'
 import { funcionality } from './src/Funcionality/Funcionality'
 import { printFooter } from './src/Footer/Footer'
+import { printForm } from './src/Form/Form'
 
 const init = async () => {
   printHeader()
   printCartas()
   funcionality()
+  printForm()
   printFooter()
 }
 init()
